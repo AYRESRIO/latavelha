a19c6ba8b657667f1be9a192699b50e0406e4c8f
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2024-11-16 09:22:40 -0300

    projet Lata Velha iniciado.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import'./styles.css';
+
+export default function Header() {
+return(
+    <header>
+        <div className="latavelha-container">
+            <h1>Lata Velha</h1>
+        </div>
+    </header>
+);
+}
